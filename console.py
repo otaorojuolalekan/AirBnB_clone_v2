dc1ac09a2353a9f89dfa9a6f90498dcce17dff1d
Ogedegbe O Eric <ohimaigold@gmail.com>
2023-06-21 14:40:52 -0700

    AirBnB update

--- console.py
+++ console.py
@@ -2,8 +2,10 @@
 """ Console Module """
 import cmd
 import sys
+from datetime import datetime
 from models.base_model import BaseModel
-from models.__init__ import storage
+from models import storage
+from shlex import split
 from models.user import User
 from models.place import Place
 from models.state import State

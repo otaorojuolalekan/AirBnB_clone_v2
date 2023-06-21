f156d07ed68f7d233c410d569c6a24c66d0f2958
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-06-21 05:42:34 +0100

    task 6 - created declarative base in models/basse_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DATETIME, String
 from datetime import datetime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60),
+                nullable=False,
+                primary_key=True,
+                unique=True)
+    created_at = Column(DATETIME,
+                        nullable=False,
+                        default=datetime.utcnow())
+    updated_at = Column(DATETIME,
+                        nullable=False,
+                        default=datetime.utcnow())
+    
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:

77b7e3c7ef48bfb928d0a010d387991ce00d7ac2
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-07-07 23:36:18 +0100

    fixed shellcheck errors

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -23,27 +23,26 @@ sudo chown -R ubuntu:ubuntu /data/
 
 # create fake file index.html
 fakebody=\
-'<!DOCTYPE html>
-<html>
+"<html>
 <head>
   <title>Welcome to Onifemi.tech</title>
 </head>
-<body style="background-color: #f2f2f2; font-family: Arial, sans-serif; text-align: center; padding: 50px;">
-  <h1 style="color: #333333;">Welcome to Onifemi.tech</h1>
-  <p style="color: #666666; font-size: 18px;">Thank you for visiting Onifemi.tech! We provide quality information and resources on various topics.</p>
-  <p style="color: #666666; font-size: 18px;">Feel free to explore the website and discover useful content.</p>
-  <p style="color: #666666; font-size: 18px;">If you have any questions, please don/t hesitate to contact us.</p>
+<body style='background-color: #f2f2f2; font-family: Arial, sans-serif; text-align: center; padding: 50px;'>
+  <h1 style='color: #333333;'>Welcome to Onifemi.tech</h1>
+  <p style='color: #666666; font-size: 18px;'>Thank you for visiting Onifemi.tech! We provide quality information and resources on various topics.</p>
+  <p style='color: #666666; font-size: 18px;'>Feel free to explore the website and discover useful content.</p>
+  <p style='color: #666666; font-size: 18px;'>If you have any questions, please don't hesitate to contact us.</p>
 </body>
-</html>'
-echo -e $fakebody > /data/web_static/releases/test/index.html
+</html>"
+echo -e "$fakebody" | sudo tee /data/web_static/releases/test/index.html
 
 # create a forced symbolic link
-ln -sf  /data/web_static/releases/test/ /data/web_static/current
+ln -sf  /data/web_static/releases/test /data/web_static/current
 
 
 
 # edit config file (append config after listen blah blah)
-sudo sed -i '/listen 80 default_server;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 # start or restart nginx service
 if [[ "$(pgrep -c nginx)" -le 0 ]]; then
